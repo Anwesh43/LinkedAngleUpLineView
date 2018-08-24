3d8575acdd4bef7fd6ca1e7ad51e9b5525b343ba
Anwesh43 <anweshthecool0@gmail.com>
2018-08-25 00:42:35 +0530

    drawing canvas extension drawAULNode

--- angleuplineview/src/main/java/com/anwesh/uiprojects/angleuplineview/AngleUpLineView.kt
+++ angleuplineview/src/main/java/com/anwesh/uiprojects/angleuplineview/AngleUpLineView.kt
@@ -9,8 +9,34 @@ import android.view.MotionEvent
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.content.Context
+import android.graphics.Color
 
 val nodes : Int = 5
+
+fun Canvas.drawAULNode(i : Int, scale : Float, paint : Paint) {
+    val h : Float = height.toFloat()
+    val w : Float = width.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = w / 3
+    var sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    val y : Float = h - (gap * i + gap / 2 + gap / 3 + gap * sc2)
+    val index : Int = i % 2
+    sc1 = sc1 * (1 - index) + (1 - sc1) * index
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.color = Color.parseColor("#4CAF50")
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w/2, y)
+    for (i in 0..1) {
+        save()
+        scale(1f - 2 * i, 1f)
+        rotate(-60f * sc1)
+        drawLine(0f, 0f, -size, 0f, paint)
+        restore()
+    }
+    restore()
+}
 
 class AngleUpLineView(ctx : Context) : View(ctx){
 
@@ -32,7 +58,7 @@ class AngleUpLineView(ctx : Context) : View(ctx){
     data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
 
         fun update(cb : (Float) -> Unit) {
-            scale += 0.1f * this.dir
+            scale += 0.05f * this.dir
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
                 dir = 0f
