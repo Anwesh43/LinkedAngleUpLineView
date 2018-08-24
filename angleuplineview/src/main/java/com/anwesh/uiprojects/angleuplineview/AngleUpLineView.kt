b20ae7e1d6019d8d4f6220c0af8ed210975a04db
Anwesh43 <anweshthecool0@gmail.com>
2018-08-25 00:50:05 +0530

    adding AngleUpLine class

--- angleuplineview/src/main/java/com/anwesh/uiprojects/angleuplineview/AngleUpLineView.kt
+++ angleuplineview/src/main/java/com/anwesh/uiprojects/angleuplineview/AngleUpLineView.kt
@@ -146,4 +146,27 @@ class AngleUpLineView(ctx : Context) : View(ctx){
             return this
         }
     }
+
+    data class AngleUpLine(var i : Int) {
+
+        private var curr : AULNode = AULNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
