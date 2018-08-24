adf9fadad3d33e0d983b4dff4ea928d9e52065b3
Anwesh43 <anweshthecool0@gmail.com>
2018-08-25 00:23:50 +0530

    importing libraries and defining number of nodes

--- angleuplineview/src/main/java/com/anwesh/uiprojects/angleuplineview/AngleUpLineView.kt
+++ angleuplineview/src/main/java/com/anwesh/uiprojects/angleuplineview/AngleUpLineView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.angleuplineview
 /**
  * Created by anweshmishra on 25/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.content.Context
+
+val nodes : Int = 5
